bd991d5f014bfc656ff06680e519dbee1a01f1f5
HenryUz <heu5@pitt.edu>
2024-03-21 11:57:34 -0400

    a

--- src/index.js
+++ src/index.js
@@ -6,26 +6,25 @@ const collection = require("./config");
 const app = express();
 
 app.use(express.json());
-
-app.use(express.urlencoded({extended: false}));
-
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
+
+// Assuming you've set the MONGO_URI environment variable on Heroku
+const mongoURI = process.env.MONGODB_URI;
+
 
 
 app.use('/login', express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-
     res.render("login");
 });
 
 app.get("/registration", (req, res) => {
-
     res.render("registration");
 });
 
-//Register user
-
+// Register user
 app.post("/registration", async (req, res) => {
     const data = {
         firstname: req.body.firstName,
@@ -39,7 +38,7 @@ app.post("/registration", async (req, res) => {
     console.log(userdata);
 })
 
-
-app.listen( process.env.PORT || 3000, () => {
+// Start the server
+app.listen(process.env.PORT, () => {
     console.log('Server is running...');
 });
