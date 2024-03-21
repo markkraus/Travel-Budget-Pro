2d5ad9ce903e2302c5bf582aac456be97f1cd56b
markkraus <91558299+markkraus@users.noreply.github.com>
2024-03-21 17:52:13 -0400

    Comments

--- src/config.js
+++ src/config.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
+
+// URI to connect to the MongoDB database      <username>  <password>
 const connect = mongoose.connect("mongodb+srv://heu5:AgDsYabct7p56ksI@cluster0.o58ssex.mongodb.net/logindata");
 
 connect.then(() => {
-    console.log("it worked");
+  // Connection was successful
+  console.log("it worked");
 })
 .catch(() =>{
-    console.log("it didnt worked");
+  // Connection was unsuccessful
+  console.log("it didnt worked");
 })
 
+// Blueprint for expected user input data to be pulled from the registration page
 const LoginSchema = new mongoose.Schema({
 
     firstname: {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+// Pulling libraries and dependencies
 const express = require('express');
 const paths = require("path");
 const bcrypt = require("bcrypt");
 const collection = require("./config");
 
+//  Creating an instance of the Express server
 const app = express();
 
+// 
 app.use(express.json());
 
 app.use(express.urlencoded({extended: false}));
@@ -13,26 +16,26 @@ app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
-//Pulls html/ejs file for default site (no extension)
-
+// Pulls html/ejs file for default site (no extension)
 app.get("/", (req, res) => {
 
     res.render("login");
 });
 
+// Pulls login page
 app.get("/login", (req, res) => {
 
     res.render("login");
 });
 
+// Pulls registration page
 app.get("/registration", (req, res) => {
-
+  
     res.render("registration");
 });
 
 
-//Register user
-
+// User get added to MongoDB database
 app.post("/registration", async (req, res) => {
     const data = {
         firstname: req.body.firstName,
@@ -42,10 +45,11 @@ app.post("/registration", async (req, res) => {
         password: req.body.password
     }
 
-    const userdata = await collection.insertMany(data);
+    const userdata = await collection.insertMany(data); // Call to add the user
     console.log(userdata);
 })
 
+// Website is being hosted either on localhost or server
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server running on Port: ${port}`);
