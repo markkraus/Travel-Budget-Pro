2180c9a7402f79ad52d1aef2a5593a58f5c60391
HenryUz <heu5@pitt.edu>
2024-03-20 21:47:24 -0400

    a

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 const express = require('express');
-const path = require('path');
+const paths = require("path");
+const bcrypt = require("bcrypt");
 const collection = require("./config");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-// Set the directory where HTML files are located
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
 
-// Define routes
+app.set('view engine', 'ejs');
+
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+
+    res.render("login");
 });
 
 app.get("/registration", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'registration.html'));
+
+    res.render("registration");
 });
 
-// Register user
+//Register user
+
 app.post("/registration", async (req, res) => {
     const data = {
         firstname: req.body.firstName,
@@ -27,14 +33,13 @@ app.post("/registration", async (req, res) => {
         email: req.body.email,
         username: req.body.username,
         password: req.body.password
-    };
+    }
 
     const userdata = await collection.insertMany(data);
     console.log(userdata);
-});
+})
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+app.listen( process.env.PORT || 3000, () => {
+    console.log('Server is running...');
 });
