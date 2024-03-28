f37901f20df69845630888f7ef0afd53ca8502a3
Ben-Mullin <91742890+Ben-Mullin@users.noreply.github.com>
2024-03-28 13:52:08 -0400

    currency tabs

--- public/spreadsheet.js
+++ public/spreadsheet.js
@@ -4,7 +4,7 @@
  */
 
 function main() {
-
+  
   // Get the container element where spreadsheet will be displayed in
   const container = document.getElementById('handsontable-container');
 
@@ -12,15 +12,59 @@ function main() {
 
   // Options for spreadsheet
   const options ={
-
-    /* data: data, */
+    //data:: data,
     rowHeaders: true, // if you want row headers
     colHeaders: ['Expense Category', 'Currency', '$Amount', 'Location', 'Date', 'Time'], // column headers
     colWidths: [150, 150, 150, 150, 150, 150], // Sets specific widths for each column
-    /* columns: [
-      // Column settings here
 
-    ], */
+    // Column settings here
+    columns: [
+
+      {
+      data: 'expenseCategory', // this should correspond to the property in your data objects
+      },
+
+      {
+        type: 'dropdown',
+        data: 'currency', // this should correspond to the property in your data objects
+        source: [
+          "USD - United States Dollar",
+          "EUR - Euro",
+          "GBP - British Pound",
+          "JPY - Japanese Yen",
+          "CNY - Chinese Yuan",
+          "INR - Indian Rupee",
+          "AUD - Australian Dollar",
+          "CAD - Canadian Dollar",
+          "SGD - Singapore Dollar",
+          "CHF - Swiss Franc",
+          "MYR - Malaysian Ringgit",
+          "NZD - New Zealand Dollar",
+          "THB - Thai Baht",
+          "RUB - Russian Ruble",
+          "ZAR - South African Rand",
+          "BRL - Brazilian Real",
+          "PHP - Philippine Peso",
+          "IDR - Indonesian Rupiah",
+          "TRY - Turkish Lira",
+          "KRW - South Korean Won",
+          "MXN - Mexican Peso",
+          "NOK - Norwegian Krone",
+        ], 
+        strict: true, // Set to true if you want to prevent users from entering values not on the list
+        allowInvalid: false, // Prevents users from entering invalid values
+      },
+
+      {
+      data: 'amount', // this should correspond to the property in your data objects
+      type: 'numeric',
+      },
+
+      {},
+      {},
+      {},
+
+    ],
     contextMenu: true,
     licenseKey: 'non-commercial-and-evaluation',
     startRows: 100, // Adjust the number of initial rows
@@ -29,6 +73,7 @@ function main() {
 
   // Initialize the spreadsheet
   const hot = new Handsontable(container, options);
+
 }
 
 // Call the main function when the DOM is fully loaded
