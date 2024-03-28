d43a298368cf8cb697bd7e7139f244ee1db94233
Ben-Mullin <91742890+Ben-Mullin@users.noreply.github.com>
2024-03-28 12:06:36 -0400

    col header fix

--- public/spreadsheet.js
+++ public/spreadsheet.js
@@ -16,7 +16,7 @@ function main() {
     /* data: data, */
     rowHeaders: true, // if you want row headers
     colHeaders: ['Expense Category', 'Currency', '$Amount', 'Location', 'Date', 'Time'], // column headers
-    colWidths: true, //[200, 100, 120, 150, 100, 100], // Sets specific widths for each column
+    colWidths: [200, 200, 200, 200, 200, 200], // Sets specific widths for each column
     /* columns: [
       // Column settings here
 
