6500682b3e4953679355977f5a67f55902a207af
markkraus <91558299+markkraus@users.noreply.github.com>
2024-03-28 09:32:53 -0400

    Modifed comments

--- public/spreadsheet.js
+++ public/spreadsheet.js
@@ -3,22 +3,18 @@
  * @author Ben Mullin
  */
 
-// Define the main function
 function main() {
 
-  
-  // Get the container element
+  // Get the container element where spreadsheet will be displayed in
   const container = document.getElementById('handsontable-container');
 
-  // Define data for the spreadsheet
   const data = [];
 
-  // Define options for Handsontable
+  // Options for spreadsheet
   const options ={
-
     data: data,
     rowHeaders: true, // if you want row headers
-    colHeaders: ['Expense Category', 'Currency', '$Amount', 'Location', 'Date', 'Time'], // column headers
+    colHeaders: ['Expense Category', 'Currency', '$Amount', 'Location', 'Date', 'Time'], // Column headers
     colWidths: [200, 100, 120, 150, 100, 100], // Sets specific widths for each column
     columns: [
       // Column settings here
@@ -26,15 +22,13 @@ function main() {
     ],
     contextMenu: true,
     licenseKey: 'non-commercial-and-evaluation',
-    startRows: 100, // Adjust the number of initial rows
-    startCols: 6, // Adjust the number of initial columns
+    startRows: 100, // Number of initial rows
+    startCols: 6, // Number of initial columns
   };
 
-  // Initialize Handsontable
+  // Initialize the spreadsheet
   const hot = new Handsontable(container, options);
-
-}//end main
-
+}
 
 // Call the main function when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', main);
