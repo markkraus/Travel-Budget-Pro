4bdad8b0d08395e1a159560e669ffab98992fcfd
Ben-Mullin <91742890+Ben-Mullin@users.noreply.github.com>
2024-03-28 12:03:38 -0400

    col header fix

--- public/spreadsheet.js
+++ public/spreadsheet.js
@@ -12,7 +12,6 @@ function main() {
 
   // Options for spreadsheet
   const options ={
-<<<<<<< HEAD
 
     /* data: data, */
     rowHeaders: true, // if you want row headers
@@ -26,16 +25,6 @@ function main() {
     licenseKey: 'non-commercial-and-evaluation',
     startRows: 100, // Adjust the number of initial rows
     startCols: 6, // Adjust the number of initial columns
-=======
-    data: data,
-    rowHeaders: true, // if you want row headers
-    colHeaders: true, //['Expense Category', 'Currency', '$Amount', 'Location', 'Date', 'Time'], // Column headers
-    //colWidths: [2000, 1000, 1200, 1500, 1000, 1000], // Sets specific widths for each column
-    // contextMenu: true,
-    // licenseKey: 'non-commercial-and-evaluation',
-    // startRows: 100, // Number of initial rows
-    // startCols: 6, // Number of initial columns
->>>>>>> 4ce96b9d886ba2603f7677a7f3dd9ccd65ed1522
   };
 
   // Initialize the spreadsheet
