2f002e73511643f538c3935e243b0a461c0be160
Ben-Mullin <91742890+Ben-Mullin@users.noreply.github.com>
2024-03-28 11:50:43 -0400

    col header fix

--- public/spreadsheet.js
+++ public/spreadsheet.js
@@ -8,18 +8,14 @@ function main() {
   // Get the container element where spreadsheet will be displayed in
   const container = document.getElementById('handsontable-container');
 
-  var data = [];
+  //var data = [];
 
   // Options for spreadsheet
   const options ={
     data: data,
     rowHeaders: true, // if you want row headers
     colHeaders: ['Expense Category', 'Currency', '$Amount', 'Location', 'Date', 'Time'], // Column headers
-    colWidths: [2000, 1000, 1200, 1500, 1000, 1000], // Sets specific widths for each column
-    //columns: [
-      // Column settings here
-
-    //],
+    //colWidths: [2000, 1000, 1200, 1500, 1000, 1000], // Sets specific widths for each column
     contextMenu: true,
     licenseKey: 'non-commercial-and-evaluation',
     startRows: 100, // Number of initial rows
